2331a86bbd1b521b2524479d1ebfc239ec16a9ce
Egor Danchenkov <egor.danchenkov@rednuclearmonkey.com>
2019-07-19 08:03:39 +0300

    Update site to app

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,8 +13,8 @@ fi
 sed -i 's+$DEVELOPERS_HOST+'"${DEVELOPERS_HOST}"'+g' /etc/nginx/sites/*.conf
 
 # Replace ENV variables in netlify config files
-sed -i 's+$WEBSITE_ROOT+'"${WEBSITE_ROOT}"'+g' /var/www/developers-site/cmsadmin/config.yml
-sed -i 's+$WEBSITE_REPO+'"${WEBSITE_REPO}"'+g' /var/www/developers-site/cmsadmin/config.yml
-sed -i 's+$AUTH_ENDPOINT+'"${AUTH_ENDPOINT}"'+g' /var/www/developers-site/cmsadmin/config.yml
+sed -i 's+$WEBSITE_ROOT+'"${WEBSITE_ROOT}"'+g' /var/www/developers-app/cmsadmin/config.yml
+sed -i 's+$WEBSITE_REPO+'"${WEBSITE_REPO}"'+g' /var/www/developers-app/cmsadmin/config.yml
+sed -i 's+$AUTH_ENDPOINT+'"${AUTH_ENDPOINT}"'+g' /var/www/developers-app/cmsadmin/config.yml
 
 nginx -c /etc/nginx/nginx.conf -g "daemon off;"
