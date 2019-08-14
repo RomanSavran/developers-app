732d15164ea475a37bc7f2706f35e1b8b86f3af9
Egor Danchenkov <egor.danchenkov@rednuclearmonkey.com>
2019-08-14 13:57:01 +0300

    Remove ununsed props

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-import PropTypes from 'prop-types';
-
 import EndToEnd from '../images/end-to-end-dx.png';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -297,12 +295,6 @@ const IndexPage = () => {
     );
 };
 
-IndexPage.propTypes = {
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            edges: PropTypes.array.isRequired
-        }).isRequired
-    }).isRequired
-};
+IndexPage.propTypes = {};
 
 export default IndexPage;
